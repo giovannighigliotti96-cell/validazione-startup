4790d3f93f98ae3431513dc3af89efa8889a4e84
Giovanni Ghigliotti <giovannighigliotti96@gmail.com>
2026-09-18 15:12:10 +0200

    pixel: PageView + Lead on the confirmation page

--- app/routers/landing.py
+++ app/routers/landing.py
@@ -81,7 +81,7 @@ def _pixel(event: str = "PageView") -> str:
         return ""
     return f"""<script>!function(f,b,e,v,n,t,s){{if(f.fbq)return;n=f.fbq=function(){{n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)}};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}}
-(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','{escape(pid)}');fbq('track','{event}');</script>"""
+(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','{escape(pid)}');fbq('track','PageView');{"fbq('track','Lead');" if event == "Lead" else ""}</script>"""
 
 
 def render(c: dict, offer: dict, v: dict, base: str, signed_up: bool = False) -> str:
